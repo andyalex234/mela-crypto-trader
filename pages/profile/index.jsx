9f573741b401671f891591140174c70a461beb65
Andenet Alexander <andyalex234@gmail.com>
2022-10-25 03:30:13 +0300

    feat: added authentication and user profile

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Button, Tabs, Space, Typography, Avatar, Image } from 'antd';
+const { Text, Link, Title } = Typography;
 
 export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
+    const { data: session } = useSession()
+    const items = [
+        {
+            label: 'User Info',
+            key: 'user-info',
+            children: (
+                <div style={{
+                    paddingLeft: "50px"
+                }}>
+                    {
+                        session && (
+                            <Space direction='vertical'>
+                                <Avatar
+                                    size={100}
+                                    src={<Image src={session.user.image} />}
+                                />
+                                <Title level={3}>{session.user.name}</Title>
+                                <Link href=''>
+                                    {session.user.email}
+                                </Link>
+                                <Button danger onClick={() => signOut()}>Sign out</Button>
+                            </Space>
+                        )
+                    }
+                </div>)
+        },
+        { label: 'Edit', key: 'edit-info', children: 'edit page' },
+        { label: 'Plans', key: 'plans', children: 'Upgrade to proplan' },
+    ];
+
+    if (session) {
+        return (
+
+            <div style={{
+                marginTop: "50px"
+            }}>
+                <Tabs
+                    tabPosition='left'
+                    items={items}
+                />
+            </div>
+        )
+    }
+    return (
+        <div>
+            Not signed in <br />
+            <Button onClick={() => signIn()}>Sign in</Button>
+        </div>
+    )
 }
